be0a6745938080922bb182e5d85892b2a203e2c1
Axel Navarro <navarroaxel@gmail.com>
2026-08-06 16:09:23 -0300

    clean up next config

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/ring",
-        destination: "/particle-dynamics/ring",
-        permanent: true,
-      },
-      {
-        source: "/kepler",
-        destination: "/particle-dynamics/kepler",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
